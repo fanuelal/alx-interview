9a5050bec863aa39ce0001f94b9542479f772844
fanuelal <fanuelalmawstop@gmail.com>
2022-04-13 13:03:29 -0700

    indent fixed

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
+""" Module that checks locked boxes """
 
 
 def canUnlockAll(boxes):
-    """ method that determines if all the boxes can be opened"""
+    """ Method that determines if all boxes can be opened """
+
+    if boxes == 0:
+        return False
 
     if not isinstance(boxes, list):
         return False
-    if boxes == 0:
-        return False
+
     if len(boxes) == 0:
         return False
-    checker = [0]
+
+    check = [0]
     list_ing = [i for i in range(len(boxes))]
 
-    for in_check in checker:
+    for in_check in check:
         for in_boxes in boxes[in_check]:
-            if in_boxes not in checker and in_boxes in list_ing:
+            if in_boxes not in check and in_boxes in list_ing:
                 if in_boxes >= len(boxes):
                     return False
-                checker.append(in_boxes)
+                check.append(in_boxes)
 
-    if len(checker) == len(boxes):
+    if len(check) == len(boxes):
         return True
     else:
         return False
