47e7ee87fec1d49084075dda85125b8e58b46f9f
fanuelal <fanuelalmawstop@gmail.com>
2022-04-24 16:58:48 -0700

    min opration fixed

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,14 +8,14 @@ def minOperations(n):
     temp = 1
     counter = 0
     cpyall = 1
+    if (n < 2):
+        return 0
     if(n % 2 != 0):
         cpyall = temp
         temp = (2 * temp)
         counter += 2
         temp += cpyall
         counter += 1
-    if (n < 2):
-        return 0
     while(temp < n):
         if ((2 * temp) <= n):
             cpyall = temp
