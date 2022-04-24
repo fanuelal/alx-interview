4cc9f41556c5a53049c85e7c8c25305fe15a8b2f
fanuelal <fanuelalmawstop@gmail.com>
2022-04-24 16:27:22 -0700

    min opration counter

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Module calculate the min num of opration"""
+
+
+def minOperations(n):
+    """ that calculates the fewest number of operations"""
+
+    temp = 1
+    counter = 0
+    cpyall = 1
+    if (n < 2):
+        return 0
+    while(temp < n):
+
+        if ((2 * temp) <= n):
+            cpyall = temp
+            temp = (2 * temp)
+            counter += 2
+        elif((temp + cpyall) <= n):
+            temp = temp + cpyall
+            counter += 1
+    return counter
