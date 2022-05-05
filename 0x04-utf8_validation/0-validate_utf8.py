2672b78440e9994ce4d71db5a7fe3d8e7d27daf7
fanuelal <fanuelalmawstop@gmail.com>
2022-05-05 12:30:12 -0700

    pycodestyle fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,7 +5,7 @@
 def validUTF8(data):
     """method that determines if a given data set
     represents a valid UTF-8 encoding"""
-    
+
     try:
         arr = [i & 255 for i in data]
         bytes(arr).decode("UTF-8")
