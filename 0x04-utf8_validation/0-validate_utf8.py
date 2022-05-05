b7ee1d5726617633059fa3a1bcdf4cb93e1b67c5
fanuelal <fanuelalmawstop@gmail.com>
2022-05-05 12:26:37 -0700

    validate utf8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module UTF-8 Validation"""
+
+
+def validUTF8(data):
+    """method that determines if a given data set
+    represents a valid UTF-8 encoding"""
+    
+    try:
+        arr = [i & 255 for i in data]
+        bytes(arr).decode("UTF-8")
+        return True
+    except Exception:
+        return False
