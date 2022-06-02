c913b4b5dc3ce595860423dd9f8589910ab3addf
fanuelal <fanuelalmawstop@gmail.com>
2022-06-02 11:46:40 -0700

    making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module Making Change"""
+
+
+def makeChange(coins, total):
+    """Return: fewest number of coins needed
+    to meet total"""
+
+    if total <= 0:
+        return 0;
+    
+    try:
+        Min = [float('inf') for i in range(total+1)]
+        Min[0] = 0
+        for i in range(1, total+1):
+            for j in range(len(coins)):
+                if Min[i - coins[j]] + 1 < Min[i]:
+                    Min[i] = Min[i - coins[j]] + 1
+
+        if Min[total] != float('inf'):
+            return Min[total]
+        else:
+            return -1
+    except Exception:
+        return -1
