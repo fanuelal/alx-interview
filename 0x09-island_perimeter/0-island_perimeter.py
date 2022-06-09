73bfd9b355747c9ee5e7c0a86e4283188a00d4a6
fanuelal <fanuelalmawstop@gmail.com>
2022-06-09 12:02:30 -0700

    assert fix

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,10 +4,17 @@
 def island_perimeter(grid):
     """that returns the perimeter of the island
     described in grid"""
+    row = len(grid)
+    col = len(grid[0])
+    assert (1 <= row and col <= 100), "length must be between 1 an 100"
+
 
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
+            assert (grid[i][j] == 0) or (grid[i][j] == 1),\
+                                        "grid numbers must be 0 or 1"
+
             count = 0
             if grid[i][j] == 1:
                 if j != (len(grid[i])-1):
