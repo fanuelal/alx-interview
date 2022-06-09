90cb8e2bad42616c13c3ed336b1e05102f6ce72d
fanuelal <fanuelalmawstop@gmail.com>
2022-06-09 12:36:28 -0700

    perimetr fix

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,34 +1,46 @@
 #!/usr/bin/python3
-"""Module island perimeter"""
+""" Module Perimeter  """
+
+
+def test0(n):
+    """
+    return 0 or 1
+    """
+    if (n == 0):
+        return 1
+    return 0
 
 
 def island_perimeter(grid):
-    """that returns the perimeter of the island
-    described in grid"""
+    """
+    returns the perimeter of the island described in grid
+    """
     row = len(grid)
     col = len(grid[0])
     assert (1 <= row and col <= 100), "length must be between 1 an 100"
-    perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
+
+    p = 0
+    for i in range(row):
+        for j in range(col):
             assert (grid[i][j] == 0) or (grid[i][j] == 1),\
                                         "grid numbers must be 0 or 1"
-
-            count = 0
             if grid[i][j] == 1:
-                if j != (len(grid[i])-1):
-                    if grid[i][j+1] == 0:
-                        count += 1
-                if j != 0:
-                    if grid[i][j-1] == 0:
-                        count += 1
-                if i != (len(grid) - 1):
-                    if grid[i+1][j] == 0:
-                        count += 1
-                if i != 0:
-                    if grid[i-1][j] == 0:
-                        count += 1
+                if i-1 < 0:
+                    p += 1
+                else:
+                    p += test0(grid[i-1][j])
+                if j-1 < 0:
+                    p += 1
+                else:
+                    p += test0(grid[i][j-1])
 
-                perimeter += count
+                try:
+                    p += test0(grid[i+1][j])
+                except IndexError:
+                    p += 1
+                try:
+                    p += test0(grid[i][j+1])
+                except IndexError:
+                    p += 1
 
-    return perimeter
+    return p
