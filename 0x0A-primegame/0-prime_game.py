e7b0cb5dd9259d63ff96d2875dbe983a32217af3
fanuelal <fanuelalmawstop@gmail.com>
2022-06-14 07:19:51 -0700

    total winner fix

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,9 +6,19 @@ def isWinner(x, nums):
     """returns the name of winner"""
     if not nums or x < 1:
         return None
+    b = 0
+    m = 0
 
     players = ["Maria", "Ben"]
     for i in range(len(nums)):
-        if nums[i] % 2 == 0:
-            return players[1]
-        return players[0]
+        if nums[i] == 1:
+            b += 1
+        elif nums[i] % 2 == 0:
+            b += 1
+        else:
+            m += 1
+    if b > m:
+        return players[1]
+    if b == m:
+        return None
+    return players[0]
